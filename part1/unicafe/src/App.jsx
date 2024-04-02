f7d51620153332913dcd7b8a3c364c367d66f26b
JusticeSwann <90804100+JusticeSwann@users.noreply.github.com>
2024-04-02 10:58:01 -0400

    unicafe 1.13

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,17 +1,26 @@
 import { useState } from 'react'
 
-const Button = ({handleClick,text}) => <button onClick={(handleClick)}>{text}</button>  //not updading buttons
-const StatisticLine  = ({text,value,unit}) => <p>{text}: {value}{unit}</p>
+const Button = ({handleStatisticClick,text}) => <button onClick={(handleStatisticClick)}>{text}</button>  //not updading buttons
+
+const StatisticLine  = ({text,value,unit}) => {
+  return(
+    <tr>
+      <td>{text}</td>
+      <td>{value}{unit}</td>
+    </tr>
+  )
+}
 const Statistics = (props) => {
   return(
-    <div>
-      <StatisticLine  text='good' value={props.good}/>
-      <StatisticLine  text='neutral' value={props.neutral}/>
-      <StatisticLine  text='bad' value={props.bad}/>
-      <StatisticLine  text='average' value={props.average}/>
-      <StatisticLine  text='positive' value={props.positive}/>
-      
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine  text='good' value={props.good}/>
+        <StatisticLine  text='neutral' value={props.neutral}/>
+        <StatisticLine  text='bad' value={props.bad}/>
+        <StatisticLine  text='average' value={props.average}/>
+        <StatisticLine  text='positive' value={props.positive}/>
+      </tbody>
+    </table>
   )
 }
 
@@ -21,11 +30,12 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [show, setShow] = useState(false)
+  const [selected, setSelected] = useState(0)
 
   const average = (good - bad)/(good+bad+neutral)
   const positive = good/(good+bad+neutral)
 
-  const handleClick = (text) => () =>{
+  const handleStatisticClick = (text) => () =>{
     if (text == 'good')
       setGood(good + 1)
     if (text === 'bad')
@@ -36,21 +46,38 @@ const App = () => {
       setShow(true) 
   }
 
-  const statistics = [good, neutral, bad, average, positive]
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const handleAnecdoteClick = () =>{
+    let updateSelected = Math.floor((Math.random()*anecdotes.length))
+    console.log(updateSelected)
+    setSelected(updateSelected)
+  }
   
 
   return (
 
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={handleClick('good')} text='good' /> {console.log(good)}
-      <Button handleClick={handleClick('neutral')} text='neutral' />
-      <Button handleClick={handleClick('bad')} text='bad' />
+      <Button handleStatisticClick={handleStatisticClick('good')} text='good' />
+      <Button handleStatisticClick={handleStatisticClick('neutral')} text='neutral' />
+      <Button handleStatisticClick={handleStatisticClick('bad')} text='bad' />
       <h1>StatisticLine </h1>
       {show && <div>
         <Statistics good = {good} bad = {bad} neutral={neutral} average={average} positive={positive}/>
       </div>}
       {!show && <p>no feedback given</p>}
+      <button onClick={handleAnecdoteClick}>next anecdote</button>
+      <p>{anecdotes[selected]}</p>
     </div>
   )
 }
